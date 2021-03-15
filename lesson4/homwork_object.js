2ca2a782e6d651e7635e2f6cc8c655907e8bfd7a
hien1812 <nguyensihien1812@gmail.com>
2021-03-15 23:05:09 +0700

    homework5

--- lesson4/homwork_object.js
+++ lesson4/homwork_object.js
@@ -29,28 +29,175 @@
 // console.log(dueDate)
 
 //bai4
-let keyW = {
-    debug: 'The process of figuring out why your program has a certain error and how to fix it',
-    done: 'When your task is complete, the only thing you have to do is to wait for users to use it (no additional codes or actions needed)',
-    perfect: 'The formal word for ‘error’',
-    pm: 'The short version of Project Manager, the person in charge of the final result of a project',
-    'ui/ux': 'UI means User Interface, UX mean User Experience, are the process to define how your products looks and feels'
+// let keyW = {
+//     debug: 'The process of figuring out why your program has a certain error and how to fix it',
+//     done: 'When your task is complete, the only thing you have to do is to wait for users to use it (no additional codes or actions needed)',
+//     perfect: 'The formal word for ‘error’',
+//     pm: 'The short version of Project Manager, the person in charge of the final result of a project',
+//     'ui/ux': 'UI means User Interface, UX mean User Experience, are the process to define how your products looks and feels'
+// }
+// alert("Hi there, this is dev dictionary")
+// let answer = prompt("Enter a keyword")
+// for(let x in keyW){
+//     if(answer == x){
+//         alert(`${x} ${keyW[x]}`)
+//     }else{
+//         let update = prompt(`We could not find your word: ${answer} leave your explanation`)
+//         alert('Done')
+//         //let nhapVao = prompt("Enter a keyword")
+//         keyW.answer = update;{
+//             alert(`${answer} 
+//             ${update}`)
+//         }
+//         break
+//     }
+// }
+
+//bai5
+let tech = [
+    {id: 1 , name : "Xiaomi portable charger 20000mah", brand : "Xiaomi",price : 428,color : "While" , Category:"Chager"},
+    {id: 2 , name : "VSmart Active 1", brand : "VSmart",price : 5487,color : "Black" , Category:"Phone"},
+    {id: 3 , name : "IPhone X", brand : "Apple",price : 21490, color : "Gray", Category:"Phone"},
+    {id: 4 ,name : "Samsung Galaxy A9", brand : "Samsung",price : 8490,color : "Blue" , Category:"Phone"},
+    
+];
+for(let tName of tech){
+    console.log(`Name : ${tName.name}`)
+    console.log(`Price : ${tName.price}`)
+    console.log("----------------------")
+
 }
-alert("Hi there, this is dev dictionary")
-let answer = prompt("Enter a keyword")
-for(let x in keyW){
-    if(answer == x){
-        alert(`${x} ${keyW[x]}`)
-    }else{
-        let update = prompt(`We could not find your word: ${answer} leave your explanation`)
-        alert('Done')
-        //let nhapVao = prompt("Enter a keyword")
-        keyW.answer = update;{
-            alert(`${answer} 
-            ${update}`)
-        }
-        break
+//5.2
+// for(let stt of tech){
+//     console.log(`#${stt.id} : ${stt.name}`)
+//     console.log(`Price : ${stt.price}`)
+// }
+// let Nhapso = prompt("enter your product position")
+// for(let x of tech){
+//     if(x.id == Nhapso){
+//         console.log(x)
+//     }
+// }
+
+//5.3
+// let answer=prompt("Enter your category?")
+// for(let x of tech){
+//     if(x.Category==answer){
+//         console.log(`Name: ${x.name}`)
+//         console.log(`Price: ${x.price}`)
+//     }
+// }
+//5.4
+tech[0].Providers = "Phukienzero Dientuccc"
+tech[1].Providers = "Tgdd Ddghn VhStore"
+tech[2].Providers = "Tgdd"
+tech[3].Providers = "Tgdd"
+for(x of tech){
+console.log(`${x.name} \n Price: ${x.price} \n Providers: ${x.Providers}`)}
+
+//5.5
+let answer1 = prompt("Enter provider")
+for(let x of tech){
+    let check = x.Providers.indexOf(answer1)
+    if(check==-1){
+        console.log('Not found')
+    }
+    else{
+        console.log(`Name : ${x.name}`)
+        console.log(`Brand : ${x.brand}`)
+        console.log(`Price : ${x.price}`)
+        console.log(`Color : ${x.color}`)
+        console.log(`Category : ${x.Category}`)
+        console.log(`Providers : ${x.Providers}`)
     }
 }
 
-//bai5
+let tasks = [
+    {
+        id: 1,
+        name: 'HTML',
+        complete: false
+    },
+    {
+        id: 2,
+        name: 'CSS',
+        complete: false
+    },
+    {
+        id: 3,
+        name: 'Basics of JavaScript',
+        complete: false
+    },
+    {
+        id: 4,
+        name: 'Node Package Manager (npm)',
+        complete: false
+    },
+    {
+        id: 5,
+        name: 'Git',
+        complete: false
+    }
+];
+
+// 6.1: In ra console
+console.log('Hi there, this is your learning tasks to front-end developer career: ');
+for(let key of tasks) {
+    console.log(`${key.id}. ${key.name}`);
+    console.log(`   Complete: ${key.complete}`);
+}
+console.log('-----------------');
+
+let inputUser = prompt('Enter your command (New, Delete, Update, Complete) ');
+// 6.2: Người dùng nhập thêm task
+if(inputUser == 'New') {
+    console.clear();
+    let nameOfNewTask = prompt('Enter new task: ');
+    nameOfNewTask = {
+        id: 6,
+        name: nameOfNewTask,
+        complete: false
+    }
+    tasks.push(nameOfNewTask);
+    console.log('Hi there, this is your learning tasks to front-end developer career: ');
+    for(let key of tasks) {
+        console.log(`${key.id}. ${key.name}`);
+        console.log(`   Complete: ${key.complete}`);
+    }
+    console.log('-------------------------------------------------------------------');
+}
+// 6.3: Người dùng sửa task
+else if(inputUser == 'Update') {
+    let position = Number(prompt('Enter position:'));
+    let title = prompt('Enter new title');
+    tasks[position].name = title;
+    console.log('Hi there, this is your learning tasks to front-end developer career: ');
+    for(let key of tasks) {
+        console.log(`${key.id}. ${key.name}`);
+        console.log(`   Complete: ${key.complete}`);
+    }
+    console.log('-------------------------------------------------------------------');       
+}
+
+// 6.4: Người dùng hoàn thành complete
+else if(inputUser == 'Complete') {
+    let position = Number(prompt('Enter position:'));
+    tasks[position - 1].complete = true;
+    console.log('Hi there, this is your learning tasks to front-end developer career: ');
+    for(let key of tasks) {
+        console.log(`${key.id}. ${key.name}`);
+        console.log(`   Complete: ${key.complete}`);
+    }
+    console.log('---------------------------');
+}
+// 6.5: Xóa task
+else(inputUser == 'Delete');{
+    let delete_position = Number(prompt('Enter position: '));
+    tasks.splice(delete_position - 1, 1);
+    console.log('Hi there, this is your learning tasks to front-end developer career: ');
+    for(let key of tasks) {
+        console.log(`${key.id}. ${key.name}`);
+        console.log(`   Complete: ${key.complete}`);
+    }
+    console.log('--------------------------');
+}
--- lesson5/lesson5.js
+++ lesson5/lesson5.js
@@ -1,199 +1,199 @@
-// let hienRose ={
-//     name: 'Hien Ro xi',
-//     address: 'Ha Noi',
-//     age: 20,
-//     isMarried: true,
-//     favourites: ["an","ngu"],
-//     wife:{
-//         name:"sap co ",
-//         age: 20
-//     },
-//     earnMoney: function(){
-//         console.log('Chi co lam moi co tien')
-//     }
-// };
-// console.log(hienRose);
-//them thuoc tinh cho 1 obj
-// hienRose["weight"] = 67;
-// hienRose.height = 175;
-// hienRose.children = [
-//     {name: "Child 1",age:10},
-//     {name: "Child 2",age:5}
-// ];
-// console.log(hienRose);
-// sua gia tri thuoc tinh
-// hienRose.name = "Hien ROxe"
-// hienRose.age = 21;
-// console.log(hienRose)
-//xoa 1 thuoc tinh
-// delete hienRose.favourites;
-// delete hienRose.address;
+let hienRose ={
+    name: 'Hien Ro xi',
+    address: 'Ha Noi',
+    age: 20,
+    isMarried: true,
+    favourites: ["an","ngu"],
+    wife:{
+        name:"sap co ",
+        age: 20
+    },
+    earnMoney: function(){
+        console.log('Chi co lam moi co tien')
+    }
+};
+console.log(hienRose);
+them thuoc tinh cho 1 obj
+hienRose["weight"] = 67;
+hienRose.height = 175;
+hienRose.children = [
+    {name: "Child 1",age:10},
+    {name: "Child 2",age:5}
+];
+console.log(hienRose);
+sua gia tri thuoc tinh
+hienRose.name = "Hien ROxe"
+hienRose.age = 21;
+console.log(hienRose)
+xoa 1 thuoc tinh
+delete hienRose.favourites;
+delete hienRose.address;
 
-//duyet object: bao nhieu thuoc tinh(giong duyet mang)
-// for(let key in hienRose){
-//     console.log("key" + key + ":" + hienRose[key]);
-// }
+duyet object: bao nhieu thuoc tinh(giong duyet mang)
+for(let key in hienRose){
+    console.log("key" + key + ":" + hienRose[key]);
+}
 
-//tao 1 object la nguoi yeu cua cac ban gom cac thuoc tinh:
-// name, age, address,weight,height,favorites[],
-//crush:{name,age,address},appearance
+tao 1 object la nguoi yeu cua cac ban gom cac thuoc tinh:
+name, age, address,weight,height,favorites[],
+crush:{name,age,address},appearance
 
-// let nguoiYeu = {
-//     name :'My Linh',
-//     age : 20,
-//     address:'Ha Noi',
-//     weight: 45,
-//     height: 165,
-//     favourite:["di choi","an uong"],
-//     crush:{
-//         name :'Hien',
-//         age : 20,
-//         address: 'Ha Noi',
-//         appearance : 'dep'
-//     }
-// }
-// console.log(nguoiYeu)
+let nguoiYeu = {
+    name :'My Linh',
+    age : 20,
+    address:'Ha Noi',
+    weight: 45,
+    height: 165,
+    favourite:["di choi","an uong"],
+    crush:{
+        name :'Hien',
+        age : 20,
+        address: 'Ha Noi',
+        appearance : 'dep'
+    }
+}
+console.log(nguoiYeu)
 
-// let data = [
-//   { id: 1, name: "Dung", age: 21, address: "HN" },
-//   { id: 2, name: "Linh", age: 20, address: "HN" },
-//   { id: 3, name: "Trang", age: 23, address: "HN" },
-//   { id: 4, name: "Thuy Anh", age: 20, address: "HN" },
-//   { id: 5, name: "Ha", age: 20, address: "Thai Nguyen" },
-//   { id: 6, name: "Phuong Anh", age: 20, address: "HN" },
-// ];
-//tim obj co id bang 3
-//tim 1 nguoi co tuoi 20
-//tim dia chi nhung nguoi hon 20 tuoi
-//tim tuoi nhung nguoi co ten bat dau bang chu L
-// for (let person of data) {
-//   if (person.id == 3) {
-//     console.log(person);
-//   }
-// }
-// for(let date of data) {
-//     if(date.age == 20) {
-//         console.log(date);
-//         break
-//     }
-// }
-//k lay phan tu o dau
-// let _name = "";
-// for(let person of data){
-//     if(person.age == 20){
-//         _name = person.name;
+let data = [
+  { id: 1, name: "Dung", age: 21, address: "HN" },
+  { id: 2, name: "Linh", age: 20, address: "HN" },
+  { id: 3, name: "Trang", age: 23, address: "HN" },
+  { id: 4, name: "Thuy Anh", age: 20, address: "HN" },
+  { id: 5, name: "Ha", age: 20, address: "Thai Nguyen" },
+  { id: 6, name: "Phuong Anh", age: 20, address: "HN" },
+];
+tim obj co id bang 3
+tim 1 nguoi co tuoi 20
+tim dia chi nhung nguoi hon 20 tuoi
+tim tuoi nhung nguoi co ten bat dau bang chu L
+for (let person of data) {
+  if (person.id == 3) {
+    console.log(person);
+  }
+}
+for(let date of data) {
+    if(date.age == 20) {
+        console.log(date);
+        break
+    }
+}
+k lay phan tu o dau
+let _name = "";
+for(let person of data){
+    if(person.age == 20){
+        _name = person.name;
         
-//     }
-// }
-// console.log(_name)
+    }
+}
+console.log(_name)
 
 
-// for(let highestAge of data) {
-//     if(highestAge.age > 20){
-//     console.log(highestAge.address)}
-// }
+for(let highestAge of data) {
+    if(highestAge.age > 20){
+    console.log(highestAge.address)}
+}
 
 
-//bai cuoi cach1
-// for(let firstName of data)  {
-//     if(firstName.name.startsWith('L',0)){
-//         console.log(firstName.name)
-//     }
-// }
+bai cuoi cach1
+for(let firstName of data)  {
+    if(firstName.name.startsWith('L',0)){
+        console.log(firstName.name)
+    }
+}
 
-//cach2
-// for(let person of data){
-//     if(person.name[0] == "L" || person.name[0] == "l"){
-//         console.log(person.age)
-//     }
-// }
+cach2
+for(let person of data){
+    if(person.name[0] == "L" || person.name[0] == "l"){
+        console.log(person.age)
+    }
+}
 
-// let films = [
-//     {
-//         id: 1,
-//         name: 'Film 1',
-//         year: 2020,
-//         actors: [
-//             'Actor 1',
-//             'Actor 2',
-//             'Actor 3'
-//         ]
-//     },
+let films = [
+    {
+        id: 1,
+        name: 'Film 1',
+        year: 2020,
+        actors: [
+            'Actor 1',
+            'Actor 2',
+            'Actor 3'
+        ]
+    },
 
-//     {
-//         id: 2,
-//         name: 'Film 2',
-//         year: 2019,
-//         actors: [
-//             'Actor 2',
-//             'Actor 3',
-//             'Actor 4'
-//         ]
-//     },
+    {
+        id: 2,
+        name: 'Film 2',
+        year: 2019,
+        actors: [
+            'Actor 2',
+            'Actor 3',
+            'Actor 4'
+        ]
+    },
 
-//     {
-//         id: 3,
-//         name: 'Film 3',
-//         year: 2021,
-//         actors: [
-//             'Actor 1',
-//             'Actor 3',
-//             'Actor 4'
-//         ]
-//     },
+    {
+        id: 3,
+        name: 'Film 3',
+        year: 2021,
+        actors: [
+            'Actor 1',
+            'Actor 3',
+            'Actor 4'
+        ]
+    },
 
-//     {
-//         id: 4,
-//         name: 'Film 4',
-//         year: 2021,
-//         actors: [
-//             'Actor 2',
-//             'Actor 1',
-//             'Actor 5'
-//         ]
-//     }
-// ];
+    {
+        id: 4,
+        name: 'Film 4',
+        year: 2021,
+        actors: [
+            'Actor 2',
+            'Actor 1',
+            'Actor 5'
+        ]
+    }
+];
 
-// 1. Tim ten cua cac film san xuat vao nam 2020
-// 2. Nhap vao ten 1 dien vien bat ki, tim cac film co dien vien do tham gia
-//=> tim phan tu co trong mang
-// 3. Sap xep danh sach film theo thu tu nam san xuat tang dan
+1. Tim ten cua cac film san xuat vao nam 2020
+2. Nhap vao ten 1 dien vien bat ki, tim cac film co dien vien do tham gia
+=> tim phan tu co trong mang
+3. Sap xep danh sach film theo thu tu nam san xuat tang dan
 
-// for(let namSanxuat of films){
-//     if(namSanxuat.year == 2020){
-//         console.log(namSanxuat.year)
-//     }
-// }
+for(let namSanxuat of films){
+    if(namSanxuat.year == 2020){
+        console.log(namSanxuat.year)
+    }
+}
 
-// let name = prompt("Nhap ten dien vien: ")
-// cach 1
-// for(let film of films){
+let name = prompt("Nhap ten dien vien: ")
+cach 1
+for(let film of films){
 
-//     let check = false;
-//     for(let actor of film.actors){
-//         if(actor == name)check = true;
-//     }
-//     if(check){
-//         console.log(film)
-//     }
-// }
+    let check = false;
+    for(let actor of film.actors){
+        if(actor == name)check = true;
+    }
+    if(check){
+        console.log(film)
+    }
+}
 
-//cach 2
-// for(let film of films){
-//     for(let actor of film.actors){
-//         if(actor == name){
-//             console.log(film)
-//             break
-//         }
-//     }
-// }
+cach 2
+for(let film of films){
+    for(let actor of film.actors){
+        if(actor == name){
+            console.log(film)
+            break
+        }
+    }
+}
 
-//cach 3
-// for(let film of films){
-//     if(film.actors.includes(name)){
-//         console.log(film)
-//     }
-// }
+cach 3
+for(let film of films){
+    if(film.actors.includes(name)){
+        console.log(film)
+    }
+}
 
 
 
@@ -201,16 +201,16 @@
 let nums = [9,7,1,2,4,10,-1,3];
 //bubble sort
 
-// for( let i =0; i< nums.length; i++){
+for( let i =0; i< nums.length; i++){
 
-//     for(let j = 0; j < nums.length; j++){
-//         if(nums[i] < nums[j]){
-//             let tmp = nums[i];
-//             nums[i] = nums[j];
-//             nums[j] = tmp;
-//         }
-//     }
-// }
+    for(let j = 0; j < nums.length; j++){
+        if(nums[i] < nums[j]){
+            let tmp = nums[i];
+            nums[i] = nums[j];
+            nums[j] = tmp;
+        }
+    }
+}
 nums.sort()
 console.log(nums)
 
