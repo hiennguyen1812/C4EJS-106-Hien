b254da78cd8cd7c83b98bbab8cd3a2cedb89aa9c
hien1812 <nguyensihien1812@gmail.com>
2021-03-27 15:45:28 +0700

    getEvent

--- lesson7/function/homeworkfunction.js
+++ lesson7/function/homeworkfunction.js
@@ -18,26 +18,62 @@
 // average(3,4,5,6,7)
 
 //bai2
-function ptbac2(a,b,c){
-    let x;
-    let delta = b*b-4*a*c;
-    if (a==0){
-        x = -c/b;
-        console.log(`Phuong trinh co nghiem duy nhat : ${x}`)
+// function ptbac2(a,b,c){
+//     let x;
+//     let delta = b*b-4*a*c;
+//     if (a==0){
+//         x = -c/b;
+//         console.log(`Phuong trinh co nghiem duy nhat : ${x}`)
+//     }
+//     else if(delta==0){
+//         x= -b/2*a
+//         console.log(`Phuong trinh co nghiem kep: ${x}`)
+//     }
+//     else if(delta>0){
+//         let x1=(-b+Math.sqrt(delta))/(2*a)
+//         let x2=(-b-Math.sqrt(delta))/(2*a)
+//         console.log(`Phương trình có 2 nghiệm phân biệt: x1=${x1} \n x2=${x2}`);
+//     }
+//     else{
+//         console.log("Phuong trinh vo nghiem")
+//     }
+// }
+// ptbac2(2,-7,3)
+
+
+/*chua bt phuong trinh bac 2
+Math.sqrt : can bac 2 cua a
+Math.pow(a,3) : a mu 3
+min(1,2,3) : 1
+max(1,2,3) : 3
+random(): 1 so trong khoang (0,1)
+floor(5.5) : 5 lam tron xuong
+
+*/
+
+//chua bai trungbinh cong
+
+let listMark=[1,1,5,3]
+getRank(averageCaculate,listMark)
+function getRank(caculate, listMark){
+    let average = caculate(listMark)
+    if(average < 5){
+        console.log("yeu")
+    }else if(average < 8){
+        console.log("kha")
+    }else{
+        console.log("pro")
     }
-    else if(delta==0){
-        x= -b/2*a
-        console.log(`Phuong trinh co nghiem kep: ${x}`)
-    }
-    else if(delta>0){
-        let x1=(-b+Math.sqrt(delta))/(2*a)
-        let x2=(-b-Math.sqrt(delta))/(2*a)
-        console.log(`Phương trình có 2 nghiệm phân biệt: x1=${x1} \n x2=${x2}`);
-    }
-    else{
-        console.log("Phuong trinh vo nghiem")
-    }
+
 }
-ptbac2(2,-7,3)
 
-
+function averageCaculate(listMark){
+    let sum = 0;
+    let average = 0
+    let len = listMark.length
+    for(let num of listMark){
+        sum += num
+    }
+    average = sum / len
+    return average
+}
---
+++ lesson8/homework/cud.js
@@ -0,0 +1,76 @@
+//bai2
+// function hee() {
+//   console.log('Hee');
+// }
+
+// setTimeout(hee, 1000);
+
+// setInterval(hee, 500);
+
+//bai4
+// let a = document.getElementsByTagName('li')
+// console.log(a[1])
+// for(let i of a){
+//   console.log(i)
+// }
+
+//bai5
+// let b_name = document.getElementsByTagName('div')
+// console.log(b_name[2])
+// for(let i of b_name){
+//   console.log(i)
+// }
+
+
+//bai8
+// function wish(){
+//   let name = "Nguyen Si Hien"
+//   let wish = "had a girfiend"
+//   alert(`I (${name}) wish I ${wish} in this year`)
+// }
+// wish()
+
+//bai9
+// function wishList(){
+//   let nhapName = prompt('Nhap ten cua ban:')
+//   let nhapDieuuoc = prompt('Nhap dieu uoc:')
+//   alert(`Name: ${nhapName} \nWish:${nhapDieuuoc}`)
+// }
+// wishList()
+
+//bai10
+// function wishList() {
+//     let inputName = prompt('Nhập tên của bạn: ');
+//     let inputWish = prompt('Nhập ước mơ của bạn: ');
+//     if(inputWish == '') {
+//         alert('Bạn chưa nhập ước mơ của bạn. ');
+//     }
+//     else {
+//         alert(`Name: ${inputName} \nWish: ${inputWish}`);
+//     }
+// }
+// wishList();
+
+//bai11
+//read button
+// let btn = document.getElementById('upper_btn')
+// console.log(btn)
+
+// //read input
+// let inputName = document.getElementById('my-input')
+// console.log(inputName);
+
+// //read div
+// let div = document.getElementById('result_div')
+// console.log(div)
+
+// btn.addEventListener('click',upperName) 
+// function upperName(clickCount){
+//     clickCount += 1 
+//     console.log("Upper it!!! Just clicked")
+//     console.log(`User's name: ${inputName.value}`);
+//     console.log(`User name uppercase: ${inputName.value.toUpperCase()} `)
+//     div.innerHTML = inputName.value.toUpperCase()
+// }
+
+
